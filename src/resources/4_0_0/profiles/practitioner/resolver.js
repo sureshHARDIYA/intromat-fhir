4c977f158448d38932c42d5906c969bdc0cdc8c8
Kolbein <kolbeinfoldoy@gmail.com>
2019-11-18 11:47:23 +0100

    Cleaner code with no unnecessary paramters etc.

--- src/resources/4_0_0/profiles/practitioner/resolver.js
+++ src/resources/4_0_0/profiles/practitioner/resolver.js
@@ -3,37 +3,20 @@
  * @static
  * @summary Practitioner resolver.
  */
-module.exports.getPractitioner = async function getPractitioner(
-	root,
-	args,
-	context = {},
-	info,
-) {
-		try {
-			const { server: { model, app }, version, req, res } = context;
-			return (await model.practitioners.getOne(args));
-		} catch (e) {
-			console.log("Practitioner error", e);
-		}
+
+module.exports.getPractitioner = async function (_, args, context = {}) {
+	return await context.server.model.practitioners.getOne(args);
 };
+
 
 /**
  * @name exports.getPractitionerList
  * @static
  * @summary Practitioner list resolver.
  */
-module.exports.getPractitionerList = async function getPractitionerList(
-	root,
-	args,
-	context = {},
-	info,
-) {
-		try {
-			const { server: { model }, version, req, res } = context;
-			return (await model.practitioners.getAll(args));
-		} catch (e) {
-			console.log("Practitioner error", e);
-		}
+
+module.exports.getPractitionerList = async function (_, args, context = {}) {
+	return await context.server.model.practitioners.getAll(args);
 };
 
 /**
@@ -41,58 +24,30 @@ module.exports.getPractitionerList = async function getPractitionerList(
  * @static
  * @summary Practitioner instance resolver.
  */
-module.exports.getPractitionerInstance = async function getPractitionerInstance(
-	root,
-	args,
-	context = {},
-	info,
-) {
-		try {
-			const { server: { model }, version, req, res } = context;
-			return (await model.practitioners.getOne(args));
-		} catch (e) {
-			console.log("Practitioner error", e);
-		}
+
+module.exports.getPractitionerInstance = async function (_, args, context = {}) {
+	return await context.server.model.practitioners.getOne(args);
 };
+
 
 /**
  * @name exports.createPractitioner
  * @static
  * @summary Create Practitioner resolver.
  */
-module.exports.createPractitioner = async function createPractitioner(
-	root,
-	args,
-	context = {},
-	info,
-) {
-		try {
-			const { server: { model }, version, req, res } = context;
-			const practitioner = await model.practitioners.createData(args.resource);
-			return (practitioner)
-		} catch (e) {
-			console.log("Practitioner error", e);
-		}
+module.exports.createPractitioner = async function (_, args, context = {}) {
+	return await context.server.model.practitioners.createData(args.resource);
 };
+
 
 /**
  * @name exports.updatePractitioner
  * @static
  * @summary Update Practitioner resolver.
  */
-module.exports.updatePractitioner = async function updatePractitioner(
-	root,
-	args,
-	context = {},
-	info,
-) {
-		try {
-			const { server: { model }, version, req, res } = context;
-			const practitioner = await model.practitioners.updateData(args.id, args.resource);
-			return (practitioner)
-		} catch(e) {
-			console.log("Practitioner error", e);
-		}
+
+module.exports.updatePractitioner = async function (_, args, context = {}) {
+	return await context.server.model.practitioners.updateData(args.id, args.resource);
 };
 
 /**
@@ -100,17 +55,6 @@ module.exports.updatePractitioner = async function updatePractitioner(
  * @static
  * @summary Remove Practitioner resolver.
  */
-module.exports.removePractitioner = async function removePractitioner(
-	root,
-	args,
-	context = {},
-	info,
-) {
-		try {
-			const { server: { model }, version, req, res } = context;
-			const practitioner = await model.practitioners.removeData(args.id);
-			return (practitioner)
-		} catch (e) {
-			console.log("Practitioner error", e);
-		}
+module.exports.removePractitioner = async function (_, args, context = {}) {
+	return await context.server.model.practitioners.removeData(args.id);
 };
